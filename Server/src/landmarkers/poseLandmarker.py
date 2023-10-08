38244919893c292c277ee92b7803b849a8527da1
stalomeow <stalowork@163.com>
2023-10-08 22:18:51 +0800

    Remove deprecated code

--- Server/src/landmarkers/poseLandmarker.py
+++ Server/src/landmarkers/poseLandmarker.py
@@ -31,39 +31,7 @@ class PoseLandmarker(Landmarker):
         return len(result.pose_landmarks) > 0
 
     def _createPacket(self, result: PoseLandmarkerResult, outputImage: mp.Image, timestampMS: int) -> Packet:
-        # b, r, t = cv2.solvePnP(
-        #     np.array([
-        #         np.array([landmark.x, 1-landmark.y, -landmark.z])
-        #         for landmark in result.pose_landmarks[0]
-        #     ]),
-        #     np.array([
-        #         np.array([landmark.x, 1 - landmark.y])
-        #         for landmark in result.pose_landmarks[0]
-        #     ]),
-        #     np.array([
-        #         [51.0, 0, 0.5],
-        #         [0, 30.0, 0.5],
-        #         [0, 0, 1]
-        #     ]),
-        #     None
-        # )
-        # print(b,r,t)
-        # x = t[0][0]
-        # y = t[1][0]
-        # z = t[2][0]
-
         poseData = PoseData()
-        # for landmark in result.pose_world_landmarks[0]:
-        #     item = poseData.landmarks.add()
-        #     # mediapipe 是倒过来的左手系
-        #     item.x = -landmark.x# - x * 0.01
-        #     item.y = -landmark.y# - y * 0.01
-        #     item.z = -landmark.z# - z * 0.01
-        for landmark in result.pose_landmarks[0]:
-            item = poseData.landmarks.add()
-            item.x = 1-landmark.x
-            item.y = 1-landmark.y
-            item.z = landmark.z
         return Packet(PacketCode.POSE_DATA, poseData.SerializeToString())
 
     def _getDrawingLandmarks(self, result: PoseLandmarkerResult):
